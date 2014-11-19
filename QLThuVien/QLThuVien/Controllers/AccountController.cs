ad48a81d481671888ca19eb83ac1f0a992d51e82
Vo Huy Viet <vietvhcr@gmail.com>
2014-11-19 23:09:22 +0700

    Do an nhom 5

---
+++ QLThuVien/QLThuVien/Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.Mvc;
+using QLThuVien.Models;
+using System.Security.Cryptography;
+using System.Text;
+namespace QLThuVien.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+        public ActionResult Login(FormCollection f)
+        {//nay ctrl c mà ra hash khác
+            string username=f["username"].ToString();
+            string password=f["password"].ToString();
+            if(username==null ||username=="" || password==null || password=="")
+            {
+                return View();
+            }
+            string passmd5 = Md5(password);
+            QuanLyThuVienEntities db = new QuanLyThuVienEntities();
+            taikhoan account = db.taikhoans.Where(a => a.Username == username).First();
+            if (account != null)
+            {
+                if(account.pass==passmd5)
+                {
+                    Session["username"]=username;
+                }
+            }
+            return RedirectToAction("Index","Home");
+        }
+        public ActionResult Logout()
+        {
+            Session.RemoveAll();
+            return RedirectToAction("Index", "Home");
+        }
+
+        static string Md5(string text)
+        {
+            MD5CryptoServiceProvider x = new MD5CryptoServiceProvider();
+            byte[] bs = Encoding.UTF8.GetBytes(text);
+            bs = x.ComputeHash(bs);
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in bs)
+            {
+                sb.Append(b.ToString("x2"));
+            }
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ QLThuVien/QLThuVien/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QLThuVien.Models;
+using System.Data.Objects;
+
+namespace QLThuVien.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult support()
+        {
+            return View();
+        }
+        public ActionResult ThongKeTheoLoaiDG()
+        {
+            QuanLyThuVienEntities data = new QuanLyThuVienEntities();
+            var So_luong_Doc_Gia = data.So_Luong_Tung_Loai_DocGia();
+            return View(So_luong_Doc_Gia);
+        }
+        public ActionResult ThongKeSuDungThuVien()
+        {
+            QuanLyThuVienEntities data = new QuanLyThuVienEntities();
+            var Doc_Gia_Su_Dung = data.ThongKeSoDocGia_TG_MuonSAP();
+            return View(Doc_Gia_Su_Dung);
+        }
+        public ActionResult ThongKeDocGiaMuonSach_AP()
+        {
+            QuanLyThuVienEntities data = new QuanLyThuVienEntities();
+            var Doc_Gia_Muon_Sach_AP = data.ThongKeChiTietMuonSAP();
+            return View(Doc_Gia_Muon_Sach_AP);
+        }
+        public ActionResult ThongKeDocGiaPhat()
+        {
+            QuanLyThuVienEntities data = new QuanLyThuVienEntities();
+            var Doc_Gia_Bi_Phat = data.ThongKePhat();
+            return View(Doc_Gia_Bi_Phat);
+        }
+
+    }
+}
